c5dbb0b0e0e489ee363ef3c3cf527cf4ba89046b
Andrew Trice <amtrice@us.ibm.com>
2022-01-21 16:22:42 -0500

    updated chart and validation script
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -50,21 +50,18 @@ else
   sleep 30
 fi
 
-DEPLOYMENT="${COMPONENT_NAME}-${BRANCH}"
 count=0
-until kubectl get deployment "${DEPLOYMENT}" -n "${NAMESPACE}" || [[ $count -eq 20 ]]; do
-  echo "Waiting for deployment/${DEPLOYMENT} in ${NAMESPACE}"
+until kubectl get sc -A ocs || [[ $count -eq 20 ]]; do
+  echo "Waiting for sc/ocs"
   count=$((count + 1))
   sleep 15
 done
 
 if [[ $count -eq 20 ]]; then
-  echo "Timed out waiting for deployment/${DEPLOYMENT} in ${NAMESPACE}"
-  kubectl get all -n "${NAMESPACE}"
+  echo "Timed out waiting for sc/ocs"
+  kubectl get sc
   exit 1
 fi
-
-kubectl rollout status "deployment/${DEPLOYMENT}" -n "${NAMESPACE}" || exit 1
 
 cd ..
 rm -rf .testrepo
