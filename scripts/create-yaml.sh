8dc1962d933e09706dd5aca5d4e2ce495335243a
Andrew Trice <amtrice@us.ibm.com>
2022-01-21 16:06:06 -0500

    added helm chart
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env bash
-
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
+CHART_DIR=$(cd "${MODULE_DIR}/chart/ocs-operator"; pwd -P)
 
 NAME="$1"
 DEST_DIR="$2"
 
+mkdir -p "${DEST_DIR}"
+
 ## Add logic here to put the yaml resource content in DEST_DIR
+
+cp -R "${CHART_DIR}"/* "${DEST_DIR}"
+
 
 find "${DEST_DIR}" -name "*"
